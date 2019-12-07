4adb891ac7814707ac1c060cff0bd2943a0117c6
Felix Hillingshaeuser <felix@mxcore.de>
2019-12-07 23:52:00 +0100

    add code for day 7 part 2

---
+++ d07-p2.go
@@ -0,0 +1,305 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	prmt "github.com/gitchander/permutation"
+)
+
+func stringArrayToIntArray(stringNumbers []string) []int {
+	var intNumbers []int
+	for _, v := range stringNumbers {
+		intNumber, _ := strconv.Atoi(v)
+		intNumbers = append(intNumbers, intNumber)
+	}
+	return intNumbers
+}
+
+func parseOptCode(instructionCode int) (int, []int) {
+	instructionString := strconv.Itoa(instructionCode)
+	instructionLength := len(instructionString)
+	opcode := 0
+	if instructionLength > 1 {
+		opcode, _ = strconv.Atoi(instructionString[instructionLength-2:])
+	} else {
+		return instructionCode, []int{0, 0, 0}
+	}
+	instructionOffset := instructionLength - 2
+	parameterModes := []string{"0", "0", "0"}
+	for i := instructionOffset; i > 0; i-- {
+		parameterModes[instructionOffset-i] = instructionString[i-1 : i]
+	}
+	return opcode, stringArrayToIntArray(parameterModes)
+}
+
+func intCodeComputer(intCode []int, offset int, input []int) (int, int, bool) {
+	output := input[0]
+	for {
+		opcode, modes := parseOptCode(intCode[offset])
+		if opcode == 99 {
+			return output, offset, true
+		} else if opcode == 1 {
+			var firstNumber, secondNumber int
+			if modes[0] == 0 {
+				firstNumber = intCode[intCode[offset+1]]
+			} else {
+				firstNumber = intCode[offset+1]
+			}
+			if modes[1] == 0 {
+				secondNumber = intCode[intCode[offset+2]]
+			} else {
+				secondNumber = intCode[offset+2]
+			}
+			intCode[intCode[offset+3]] = firstNumber + secondNumber
+			offset += 4
+		} else if opcode == 2 {
+			var firstNumber, secondNumber int
+			if modes[0] == 0 {
+				firstNumber = intCode[intCode[offset+1]]
+			} else {
+				firstNumber = intCode[offset+1]
+			}
+			if modes[1] == 0 {
+				secondNumber = intCode[intCode[offset+2]]
+			} else {
+				secondNumber = intCode[offset+2]
+			}
+			intCode[intCode[offset+3]] = firstNumber * secondNumber
+			offset += 4
+		} else if opcode == 3 {
+			if len(input) < 1 {
+				fmt.Println("too few inputs")
+				os.Exit(1)
+			}
+			intCode[intCode[offset+1]] = input[0]
+			input = input[1:]
+			offset += 2
+		} else if opcode == 4 {
+			if modes[0] == 0 {
+				output = intCode[intCode[offset+1]]
+			} else {
+				output = intCode[offset+1]
+			}
+			offset += 2
+			return output, offset, false
+		} else if opcode == 5 {
+			if modes[0] == 0 {
+				if intCode[intCode[offset+1]] != 0 {
+					if modes[1] == 0 {
+						offset = intCode[intCode[offset+2]]
+					} else {
+						offset = intCode[offset+2]
+					}
+				} else {
+					offset += 3
+				}
+			} else {
+				if intCode[offset+1] != 0 {
+					if modes[1] == 0 {
+						offset = intCode[intCode[offset+2]]
+					} else {
+						offset = intCode[offset+2]
+					}
+				} else {
+					offset += 3
+				}
+			}
+		} else if opcode == 6 {
+			if modes[0] == 0 {
+				if intCode[intCode[offset+1]] == 0 {
+					if modes[1] == 0 {
+						offset = intCode[intCode[offset+2]]
+					} else {
+						offset = intCode[offset+2]
+					}
+				} else {
+					offset += 3
+				}
+			} else {
+				if intCode[offset+1] == 0 {
+					if modes[1] == 0 {
+						offset = intCode[intCode[offset+2]]
+					} else {
+						offset = intCode[offset+2]
+					}
+				} else {
+					offset += 3
+				}
+			}
+		} else if opcode == 7 {
+			if modes[0] == 0 && modes[1] == 0 {
+				if intCode[intCode[offset+1]] < intCode[intCode[offset+2]] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			} else if modes[0] == 0 && modes[1] == 1 {
+				if intCode[intCode[offset+1]] < intCode[offset+2] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			} else if modes[0] == 1 && modes[1] == 0 {
+				if intCode[offset+1] < intCode[intCode[offset+2]] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			} else if modes[0] == 1 && modes[1] == 1 {
+				if intCode[offset+1] < intCode[offset+2] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			}
+			offset += 4
+		} else if opcode == 8 {
+			if modes[0] == 0 && modes[1] == 0 {
+				if intCode[intCode[offset+1]] == intCode[intCode[offset+2]] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			} else if modes[0] == 0 && modes[1] == 1 {
+				if intCode[intCode[offset+1]] == intCode[offset+2] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			} else if modes[0] == 1 && modes[1] == 0 {
+				if intCode[offset+1] == intCode[intCode[offset+2]] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			} else if modes[0] == 1 && modes[1] == 1 {
+				if intCode[offset+1] == intCode[offset+2] {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 1
+					} else {
+						intCode[offset+3] = 1
+					}
+				} else {
+					if modes[2] == 0 {
+						intCode[intCode[offset+3]] = 0
+					} else {
+						intCode[offset+3] = 0
+					}
+				}
+			}
+			offset += 4
+		} else {
+			fmt.Println("error")
+			os.Exit(1)
+		}
+	}
+}
+
+func main() {
+	file, _ := os.Open("d07-input.txt")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		intCodeStringArray := strings.Split(scanner.Text(), ",")
+		intCode := stringArrayToIntArray(intCodeStringArray)
+
+		var max int
+
+		numbers := []int{5, 6, 7, 8, 9}
+		permutations := prmt.New(prmt.IntSlice(numbers))
+		for permutations.Next() {
+			result := 0
+			var intCodes [5][]int
+			var offsets [5]int
+			for i := 0; i < 5; i++ {
+				intCodes[i] = append(intCodes[i], intCode...)
+				offsets[i] = 0
+			}
+
+			for i := 0; i < 5; i++ {
+				if i == 0 {
+					result, offsets[i], _ = intCodeComputer(intCodes[i], offsets[i], []int{numbers[i], 0})
+				} else {
+					result, offsets[i], _ = intCodeComputer(intCodes[i], offsets[i], []int{numbers[i], result})
+				}
+			}
+
+			done := false
+			i := 0
+			for {
+				if i == 5 {
+					i = 0
+				}
+				result, offsets[i], done = intCodeComputer(intCodes[i], offsets[i], []int{result})
+				if done {
+					break
+				}
+				i++
+			}
+
+			if result > max {
+				max = result
+			}
+		}
+		fmt.Println(max)
+	}
+}
